491b9607ca03869ef602157eea121f0add37b655
Scott Browne <Smiles@scotts-macbook-pro.local>
2018-02-02 12:53:30 +0000

    v0.3 Hotfix.
    -Filter Example Cell file was not pushed correctly.

---
+++ ImageEditor/Viewcontrollers/IEFilterExampleCell.swift
@@ -0,0 +1,17 @@
+//
+//  IEFilterExampleCell.swift
+//  ImageEditor
+//
+//  Created by Scott Browne on 31/01/2018.
+//  Copyright © 2018 Smiles Dev. All rights reserved.
+//
+
+import UIKit
+
+class IEFilterExampleCell: UICollectionViewCell {
+    
+    @IBOutlet weak var filterNameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
+
