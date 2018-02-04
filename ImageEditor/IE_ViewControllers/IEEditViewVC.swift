0bdc7ff3cff6aa1736bb5b4c2066e36217ad232a
Scott Browne <Smiles@scotts-macbook-pro.local>
2018-02-04 20:25:14 +0000

    V1.1
    General:
    -Added comments annotating code.
    -Changed app display name.

--- ImageEditor/IE_ViewControllers/IEEditViewVC.swift
+++ ImageEditor/IE_ViewControllers/IEEditViewVC.swift
@@ -9,6 +9,7 @@
 import UIKit
 import CoreImage
 
+// A simple object to help manage the filter collection view
 struct IEFilter {
     
     var name: String
@@ -18,6 +19,7 @@ struct IEFilter {
 
 class IEEditViewVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UIGestureRecognizerDelegate {
     
+//  --- Setting initial variables. ---
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var intensityControl: UISlider!
     @IBOutlet weak var collectionView: UICollectionView!
@@ -30,17 +32,17 @@ class IEEditViewVC: UIViewController, UICollectionViewDelegate, UICollectionView
     var currentColor: UIColor!
     
     var selectedFilter = ""
-    
     var filterArray = [IEFilter]()
     
+// --- This is called when the view appears, running any initial code required. ---
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = currentColor
-        
         imageView.layer.cornerRadius = ((imageView.bounds.width-14)/8)
         imageView.clipsToBounds = true
         
+        //This code sets up the initil filter when the view is loaded.
         context = CIContext()
         currentFilter = filterArray[0].filter
         let beginImage = CIImage(image: currentImage)
@@ -49,6 +51,7 @@ class IEEditViewVC: UIViewController, UICollectionViewDelegate, UICollectionView
         applyProcessing()
     }
     
+// --- This function applies the selected filter to the CIContext, as well as enabling/disabling the intensity control bar as required.
     func applyProcessing() {
         
         let inputKeys = currentFilter.inputKeys
@@ -61,7 +64,7 @@ class IEEditViewVC: UIViewController, UICollectionViewDelegate, UICollectionView
         }
     
         if inputKeys.contains(kCIInputRadiusKey) {
-            currentFilter.setValue(intensityControl.value, forKey: kCIInputRadiusKey)
+            currentFilter.setValue((intensityControl.value * 2000), forKey: kCIInputRadiusKey)
             intensityControl.isHidden = false
         }
         
@@ -72,10 +75,14 @@ class IEEditViewVC: UIViewController, UICollectionViewDelegate, UICollectionView
         }
     }
     
+    
+// --- This sets the number of cells that shold be in the collection view at any time. ---
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return filterArray.count
     }
+  
     
+// --- This populates the collection view with the available filters.
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FilterCell", for: indexPath) as! IEFilterExampleCell
@@ -89,38 +96,39 @@ class IEEditViewVC: UIViewController, UICollectionViewDelegate, UICollectionView
         
     }
     
+// --- This controls what happens when the user selects a filter in the collection view. ---
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         guard currentImage != nil else { return }
-        
         self.selectedFilter = filterArray[indexPath.item].name
-        
         self.currentFilter = filterArray[indexPath.item].filter
-        
         let updatedImage = CIImage(image: currentImage)
         currentFilter.setValue(updatedImage, forKey: kCIInputImageKey)
         
         applyProcessing()
     }
     
+// --- This code has the intensity bar apply the varying filter intensity dynamically. ---
     @IBAction func intensityChagned(_ sender: Any) {
         applyProcessing()
     }
     
+// --- This functions presents the iOS share sheet, allowing the user to share the image in it's current state. ---
     func displayShareSheet(shareContent:UIImage) {
         let activityViewController = UIActivityViewController(activityItems: [shareContent as UIImage], applicationActivities: nil)
         present(activityViewController, animated: true, completion: {})
     }
     
+// --- This calls the share sheet function when the share button is pressed. ---
     @IBAction func shareButton(_ sender: Any) {
-        
         displayShareSheet(shareContent: imageToShare!)
-        
     }
-
+    
+// --- This dismisses the Edit View VC when the user swipes right. ---
     @IBAction func backSwipe(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
+
+// --- This function creates the filter array that feeds into the collection view. ---
     func makeFilterList() {
         
         filterArray.append(IEFilter.init(name: "Sepia", filter: CIFilter(name: "CISepiaTone")!))
--- ImageEditor/IE_ViewControllers/IEMainVC.swift
+++ ImageEditor/IE_ViewControllers/IEMainVC.swift
@@ -8,38 +8,25 @@
 
 import UIKit
 
+// This is the structure for parsing the JSON fed back from our API calls.
 struct RedditPageJSON: Codable {
-    
-    let data: data
     let kind: String
-    
+    let data: data
     struct data: Codable {
-        
         let after: String
         let children: Array<data>
-        
         struct data: Codable {
-            
             let data: redditPost
-            
             struct redditPost: Codable {
-                
                 let url: String
                 let preview: imageData?
-                
                 struct imageData: Codable {
-                    
                     let images: Array<data>
-                    
                     struct data: Codable {
-                        
                         let source: data
                         let resolutions: Array<data>
-                        
                         struct data: Codable {
-                            
                             let url: String
-                            
                         }
                     }
                 }
@@ -48,6 +35,7 @@ struct RedditPageJSON: Codable {
     }
 }
 
+//This is a simple object for storing a preview image and a high resolution image URL together.
 class LoadedImage {
     let lowResImage: UIImage!
     let sourceResImageURL: String!
@@ -60,10 +48,11 @@ class LoadedImage {
 
 class IEMainVC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UIScrollViewDelegate {
     
+//  --- Setting initial variables. ---
     @IBOutlet weak var collectionView: UICollectionView!
     
     var imageArray = [LoadedImage]()
-    var subredditName = "HumanPorn"
+    var subredditName = "HumanPorn" // The word 'Human' can be changed to any of around 100 different subect matters, and will change the content that appears. (Examples: Motorcycle, Earth, Architecture, Space, Food...)
     var lastImage = ""
     
     let minColorHue: Float = 0.0
@@ -73,23 +62,124 @@ class IEMainVC: UIViewController, UICollectionViewDataSource, UICollectionViewDe
     var bgCount: Float = 0.0
     var bgColor = UIColor(hue: 0.0, saturation: 0.5, brightness: 0.8, alpha: 1.0)
     
+    
+// --- This is called when the view appears, running any initial code required. ---
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //This code sets the initial background color for the view and ensures that the collection view fits two equally sized cells side by side symettrically.
         collectionView.backgroundColor = UIColor(hue: 0.0, saturation: 0.5, brightness: 0.7, alpha: 1.0)
         let layout = self.collectionView.collectionViewLayout as! UICollectionViewFlowLayout
         let itemWidth = ((collectionView.bounds.width-30)/2)
         let itemHeight = ((collectionView.bounds.width-30)/2)
         layout.itemSize = CGSize(width: itemWidth, height:itemHeight)
-        
+
         getImages()
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    
+    
+    
+// --- This sets the number of cells that shold be in the collection view at any time. ---
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+
+// --- This function adds new cells to the colelction view whenever new content is available. ---
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath) as! IEImageCell
+        cell.layer.cornerRadius = ((collectionView.bounds.width-30)/8)
+        cell.cellImage.image = self.imageArray[indexPath.item].lowResImage
+        
+        return cell
+        
+    }
+    
+    
+//  --- This function fetches more images to display to the user when the approach the last of the pre loaded content. ---
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if indexPath.item == (imageArray.count-10) {
+            getImages()
+        }
+    }
+    
+    
+    
+//  --- This code controls what happens when the user selects a cell. ---
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let secondViewController = storyboard.instantiateViewController(withIdentifier: "EditViewVC") as! IEEditViewVC
+        
+        //The following block loads the full resolution image, so it can be presented on the following view controller.
+        let imgURLString = imageArray[indexPath.item].sourceResImageURL
+        let editedString = imgURLString?.replacingOccurrences(of: "amp;", with: "")
+        do{
+            let imgData = try Data(contentsOf: NSURL(string: editedString!)! as URL)
+            let img = UIImage(data: imgData)
+            secondViewController.currentImage = img
+        
+        }catch let imgErr{
+            print("Error loading Img:", imgErr)
+            secondViewController.currentImage = imageArray[indexPath.item].lowResImage
+        }
+        
+        //This section defines how the Edit View VC looks when it's presented, the background color of the view is the same as the Main VCs current background color.
+        secondViewController.currentColor = bgColor
+        secondViewController.makeFilterList()
+        secondViewController.modalTransitionStyle = UIModalTransitionStyle.flipHorizontal
+        self.present(secondViewController, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+    
+//   --- This code controls the background color as the user scrolls. ---
+    
+    // When the user scrolls, theis function will call and determine how far the view has been scrolled...
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let maximumVerticalOffset = scrollView.contentSize.height - scrollView.frame.height
+        let currentVerticalOffset = scrollView.contentOffset.y
+        let percentageVerticalOffset = currentVerticalOffset / maximumVerticalOffset
+
+        didScrollToPercentageOffset(percentOffset: Float(percentageVerticalOffset))
+    }
+    
+    // ...This function is then called, which sets some variables to keep a track of how far has been scrolled in total, then sets the new backgorund color, using...
+    func didScrollToPercentageOffset(percentOffset: Float){
+        bgTotalPercent = bgTotalPercent + percentOffset
+        bgCount = bgTotalPercent - round(Float(bgTotalPercent))
+        
+        self.collectionView.backgroundColor = colorForOffsetPercentage()
+    }
+    
+    // ...This function to generate the appropriate color, based on the distance scrolled.
+    func colorForOffsetPercentage() -> UIColor {
+        let percentage = (bgTotalPercent - bgCount) * 0.01
+        
+        let actualHue = CGFloat.init((maxColorHue - minColorHue) * percentage + minColorHue)
+        
+        let color = UIColor(hue: actualHue, saturation: 0.5, brightness: 0.7, alpha: 1.0)
+        
+        bgColor = color
+        
+        return color
+    }
+    
+    
+    
+    
+    
+    
+    
+//  --- The following function loads the initial images to be displayed when the app is launched, it also sets a variable to use as a pagination reference when making consecutive calls, this allows the function to be called whenever more images are required.
+//  --- The images loaded are a much lower resolution than the source, to optimise loading time, but we alo save the URL of the source image, so it can be loaded as necessary. ---
     func getImages(){
         
         var urlString = ""
@@ -148,90 +238,6 @@ class IEMainVC: UIViewController, UICollectionViewDataSource, UICollectionViewDe
             }
             
         }.resume()
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imageArray.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath) as! IEImageCell
-
-        cell.layer.cornerRadius = ((collectionView.bounds.width-30)/8)
-        
-        cell.cellImage.image = self.imageArray[indexPath.item].lowResImage
-        
-        return cell
-        
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        
-        if indexPath.item == (imageArray.count-10) {
-            getImages()
-        }
-        
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let secondViewController = storyboard.instantiateViewController(withIdentifier: "EditViewVC") as! IEEditViewVC
-        
-        let imgURLString = imageArray[indexPath.item].sourceResImageURL
-        let editedString = imgURLString?.replacingOccurrences(of: "amp;", with: "")
-        do{
-            let imgData = try Data(contentsOf: NSURL(string: editedString!)! as URL)
-            let img = UIImage(data: imgData)
-            secondViewController.currentImage = img
-        
-        }catch let imgErr{
-            print("Error loading Img:", imgErr)
-            secondViewController.currentImage = imageArray[indexPath.item].lowResImage
-        }
-        
-        secondViewController.currentColor = bgColor
-        secondViewController.makeFilterList()
-        secondViewController.modalTransitionStyle = UIModalTransitionStyle.flipHorizontal
-
-        self.present(secondViewController, animated: true, completion: nil)
-        
-    }
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        
-        let maximumVerticalOffset = scrollView.contentSize.height - scrollView.frame.height
-        let currentVerticalOffset = scrollView.contentOffset.y
-        
-        let percentageVerticalOffset = currentVerticalOffset / maximumVerticalOffset
-        
-        didScrollToPercentageOffset(percentOffset: Float(percentageVerticalOffset))
-        
-    }
-    
-    
-    func didScrollToPercentageOffset(percentOffset: Float){
-    
-        self.collectionView.backgroundColor = colorForOffsetPercentage()
-        
-        bgTotalPercent = bgTotalPercent + percentOffset
-        bgCount = bgTotalPercent - round(Float(bgTotalPercent))
-
-    }
-    
-    func colorForOffsetPercentage() -> UIColor {
-        
-        let percentage = (bgTotalPercent - bgCount) * 0.01
-        
-        let actualHue = CGFloat.init((maxColorHue - minColorHue) * percentage + minColorHue)
-        
-        let color = UIColor(hue: actualHue, saturation: 0.5, brightness: 0.7, alpha: 1.0)
-        
-        bgColor = color
-        
-        return color
-        
     }
     
 }
